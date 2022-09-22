866751b5b3b71213be89ed992ea44a6f6ca9bfca
Eren Budur <erenbudur33@gmail.com>
2022-09-22 17:55:12 -0400

    Added working read_csv using regex

--- labs/02-lab/cs506/read.py
+++ labs/02-lab/cs506/read.py
@@ -1,6 +1,22 @@
+import re
+from tokenize import Double
+
+def test_num(value):
+    if re.match(r'^\d+$', value) :
+        return int(value)
+    if re.match(r'^-?\d+(?:\.\d+)$', value) :
+        return float(value)
+    return value
+
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    with open(csv_file_path, 'r') as f:
+        results = []
+        for line in f:
+                row = line.rstrip().split(',')
+                results.append(list(map(lambda x: test_num(x),row)))
+
+        return results    
